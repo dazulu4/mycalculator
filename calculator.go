9f3ab48af3b324ae38215c5bcd7c641b9c1d9ecf
Denis Alexander Zuluaga Quintero <dazulu4@gmail.com>
2023-02-13 01:31:44 -0500

    public resources

--- calculator.go
+++ calculator.go
@@ -8,13 +8,13 @@ import (
 	"strings"
 )
 
-type calc struct {
+type Calc struct {
 }
 
-func (calc) operate(input string, operation string) int {
+func (Calc) Operate(input string, operation string) int {
 	cleanInput := strings.Split(input, operation)
-	operator1 := parser(cleanInput[0])
-	operator2 := parser(cleanInput[1])
+	operator1 := Parser(cleanInput[0])
+	operator2 := Parser(cleanInput[1])
 	switch operation {
 	case "+":
 		// fmt.Println(operator1 + operator2)
@@ -34,7 +34,7 @@ func (calc) operate(input string, operation string) int {
 	}
 }
 
-func parser(input string) int {
+func Parser(input string) int {
 	operator, err := strconv.Atoi(input)
 	if err != nil {
 		fmt.Println(err)
@@ -42,15 +42,15 @@ func parser(input string) int {
 	return operator
 }
 
-func readInput() string {
+func ReadInput() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return scanner.Text()
 }
 
-func main() {
-	input := readInput()
-	operator := readInput()
-	c := calc{}
-	fmt.Println(c.operate(input, operator))
-}
+// func main() {
+// 	input := readInput()
+// 	operator := readInput()
+// 	c := calc{}
+// 	fmt.Println(c.operate(input, operator))
+// }
